5f856232f4e17dc87db526cc1649ba3d27888e92
sulemanahmedrajput <sulemanahmed2704@gmail.com>
2024-10-10 16:51:27 +0500

    initial project

---
+++ app/signup/page.tsx
@@ -0,0 +1,11 @@
+const SignupForm = () => {
+  return (
+    <form>
+      <input type="text" name="email" placeholder="Email" />
+      <input type="text" name="username" placeholder="Username" />
+      <input type="text" name="password" placeholder="Password" />
+    </form>
+  );
+};
+
+export default SignupForm;
